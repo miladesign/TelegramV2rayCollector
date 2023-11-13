15343e8e37074c740e0af3cc8ecb4667ecdb4643
miladesign <rezagah.milad@gmail.com>
2023-11-13 09:15:51 +0330

    fixed

--- modules/ipinfo.php
+++ modules/ipinfo.php
@@ -19,10 +19,10 @@ function ip_info($ip) {
 
     // List of API endpoints
     $endpoints = [
-        'https://api.ipbase.com/v1/json/{ip}',
         'https://ipapi.co/{ip}/json/',
         'https://ipwhois.app/json/{ip}',
-        'http://www.geoplugin.net/json.gp?ip={ip}'
+        'http://www.geoplugin.net/json.gp?ip={ip}',
+        'https://api.ipbase.com/v1/json/{ip}'
     ];
 
     // Initialize an empty result object
@@ -51,23 +51,22 @@ function ip_info($ip) {
 
             // Extract relevant information and update the result object
             if ($endpoint == $endpoints[0]) {
-                // Data from ipbase.com
-                $result->country_code = $data->country_code ?? "XX";
-                $result->country_name = $data->country_name ?? "UNKNOWN";
-            } elseif ($endpoint == $endpoints[1]) {
                 // Data from ipapi.co
                 $result->country_code = $data->country_code ?? "XX";
                 $result->country_name = $data->country_name ?? "UNKNOWN";
-            } elseif ($endpoint == $endpoints[2]) {
+            } elseif ($endpoint == $endpoints[1]) {
                 // Data from ipwhois.app
                 $result->country_code = $data->country_code ?? "XX";
                 $result->country_name = $data->country ?? "UNKNOWN";
-            } elseif ($endpoint == $endpoints[3]) {
+            } elseif ($endpoint == $endpoints[2]) {
                 // Data from geoplugin.net
                 $result->country_code = $data->geoplugin_countryCode ?? "XX";
                 $result->country_name = $data->geoplugin_countryName ?? "UNKNOWN";
+            } elseif ($endpoint == $endpoints[3]) {
+                // Data from ipbase.com
+                $result->country_code = $data->country_code ?? "XX";
+                $result->country_name = $data->country_name ?? "UNKNOWN";
             }
-
             // Break out of the loop since we found a successful endpoint
             break;
         }
